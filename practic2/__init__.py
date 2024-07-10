7f97acc749b6a1e18656eccc5b76db04bd287242
Tisha1810 <itisha65@gmail.com>
2024-07-10 15:20:52 +0300

    Practic no 2

---
+++ practic2/__init__.py
@@ -0,0 +1,19 @@
+orderNumber = 1
+client = 'Boris'
+product = 'smartphone'
+price = 349.99
+adress = 'Baker street, 29'
+print('Order No:' + str(orderNumber) + ' Client: ' + client)
+print('Product: ' + product + ' Price: ' + str(price) + 'EUR')
+print('Adress: ' + adress)
+
+print('---------')
+
+orderNumber = 2
+client = 'Doris'
+product = 'PlayStation'
+price = 349.99 + 200
+adress = 'Moon street, 29'
+print('Order No:' + str(orderNumber) + ' Client: ' + client)
+print('Product: ' + product + ' Price: ' + str(price) + 'EUR')
+print('Adress: ' + adress)
